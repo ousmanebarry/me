351e5582b2b087b922f7054ba87c71fe3fd6dc04
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-12-30 11:54:31 -0500

    Display links

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { data } from './data';
 
 const Main = () => {
 	return (
-		<div>
-			<div className='col1'></div>
-			<div className='col2'></div>
+		<div className='main'>
+			<div className='col1'>
+				{data.map((datum) => {
+					const { name, link, target, rel } = datum;
+					return (
+						<a className={name} href={link} target={target} rel={rel}>
+							{name}
+						</a>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
