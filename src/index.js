07e7bb307d0ddfebe293e2822bafeadef31de21b
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-12-30 09:51:39 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+	return <></>;
+}
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
