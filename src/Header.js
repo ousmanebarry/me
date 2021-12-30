254b132f3feb58a4021c1d1db7e5d84d628a63b8
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-12-30 14:22:32 -0500

    Added profile picture

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,12 @@ import React from 'react';
 const Header = () => {
 	return (
 		<>
+			<div className='prof-pic'>
+				<img
+					src='https://avatars.githubusercontent.com/u/79618101?v=4'
+					alt=''
+				/>
+			</div>
 			<h1>Ousmane Barry</h1>
 			<p>Software Engineering Student at the University of Ottawa</p>
 		</>
