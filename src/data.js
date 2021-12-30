27021e956c81127ccfbfd9d778bf8a1bd818b95c
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-12-30 11:54:14 -0500

    Added links data

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+	{
+		name: 'Website',
+		link: 'https://ousmanebarry.ca/',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+	},
+	{
+		name: 'Github',
+		link: 'https://github.com/ousmanebarry/',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+	},
+	{
+		name: 'LinkedIn',
+		link: 'https://www.linkedin.com/in/barry-ousmane/',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+	},
+	{
+		name: 'Mail',
+		link: 'mailto:kegnekobarry822@gmail.com',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+	},
+];
