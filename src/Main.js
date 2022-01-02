44cce1c99be9d59eb70f4948011710c4d53ad454
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2022-01-02 03:01:11 -0500

    Add 'view source code'

--- src/Main.js
+++ src/Main.js
@@ -32,6 +32,20 @@ const Main = () => {
 							);
 						}
 					})}
+					<a
+						href='https://github.com/ousmanebarry/me'
+						target='_blank'
+						rel='noopener noreferrer'
+						className='source-code'
+					>
+						<img
+							src='https://cdn.discordapp.com/attachments/439833184260063233/926272727260741722/linking_64px.png'
+							alt='Redirect Link'
+							width='40px'
+							height='40px'
+						/>
+						View Source Code
+					</a>
 				</div>
 			</div>
 		</>
