5431dfa88f61f824fa5be62670f28fb00361f5a2
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2022-01-03 01:48:29 -0500

    Change profile picture

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
 			<div className='prof-pic'>
 				<img
 					className='img'
-					src='https://avatars.githubusercontent.com/u/79618101?v=4'
-					alt=''
+					src='https://media-exp1.licdn.com/dms/image/C4E03AQF1PlpXu73fwg/profile-displayphoto-shrink_100_100/0/1641192120457?e=1646870400&v=beta&t=MoGA7qCv5XgOOgp1zg0CR7sPjiI12SQpSxe6CJfJT-s'
+					alt='Profile'
 				/>
 			</div>
 			<h1>Ousmane Barry</h1>
