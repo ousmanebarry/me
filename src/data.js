f22c560ffae641aaa010b81f82ef1e6f97405cea
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2022-01-04 18:10:51 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,65 +1,58 @@
-export const data = [
+export const links = [
 	{
 		id: 1,
-		name: 'Links',
-		classname: 'link-underline',
-	},
-	{
-		id: 2,
 		name: 'Portfolio',
 		link: 'https://ousmanebarry.ca/',
 		target: '_blank',
 		rel: 'noopener noreferrer',
 	},
 	{
-		id: 3,
+		id: 2,
 		name: 'Github',
 		link: 'https://github.com/ousmanebarry/',
 		target: '_blank',
 		rel: 'noopener noreferrer',
 	},
 	{
-		id: 4,
+		id: 3,
 		name: 'LinkedIn',
 		link: 'https://www.linkedin.com/in/barry-ousmane/',
 		target: '_blank',
 		rel: 'noopener noreferrer',
 	},
 	{
-		id: 5,
+		id: 4,
 		name: 'Mail',
 		link: 'mailto:kegnekobarry822@gmail.com',
 		target: '_blank',
 		rel: 'noopener noreferrer',
 	},
+];
+
+export const projects = [
 	{
 		id: 6,
-		name: 'Featured Projects',
-		classname: 'project-underline',
-	},
-	{
-		id: 7,
 		name: 'Wiki Search',
 		link: 'https://wiki-search-web.netlify.app/',
 		target: '_blank',
 		rel: 'noopener noreferrer',
 	},
 	{
-		id: 8,
+		id: 7,
 		name: 'Random Quote Generator',
 		link: 'https://random-quote-generator-web.netlify.app/',
 		target: '_blank',
 		rel: 'noopener noreferrer',
 	},
 	{
-		id: 9,
+		id: 8,
 		name: 'Akinator Discord',
 		link: 'https://github.com/ousmanebarry/akinator-discord',
 		target: '_blank',
 		rel: 'noopener noreferrer',
 	},
 	{
-		id: 10,
+		id: 9,
 		name: 'Typeracer Bot',
 		link: 'https://github.com/ousmanebarry/typeracer-bot',
 		target: '_blank',
