a1502b7b2df8e65a311fec45967bc5303859022c
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2022-01-04 18:10:54 -0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,13 +3,6 @@ import React from 'react';
 const Header = () => {
 	return (
 		<>
-			<div className='prof-pic'>
-				<img
-					className='img'
-					src='https://media-exp1.licdn.com/dms/image/C4E03AQF1PlpXu73fwg/profile-displayphoto-shrink_100_100/0/1641192120457?e=1646870400&v=beta&t=MoGA7qCv5XgOOgp1zg0CR7sPjiI12SQpSxe6CJfJT-s'
-					alt='Profile'
-				/>
-			</div>
 			<h1>Ousmane Barry</h1>
 			<p>Software Engineering Student at the University of Ottawa</p>
 		</>
