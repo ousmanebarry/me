91f8ae024e2318a623fee6791824585eff5a30a6
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2022-01-04 18:09:59 -0500

    Remove manifest.json

--- src/Main.js
+++ src/Main.js
@@ -1,37 +1,46 @@
 import React from 'react';
-import { data } from './data';
+import { links, projects } from './data';
 
 const Main = () => {
 	return (
 		<>
 			<div className='main'>
 				<div className='col1'>
-					{data.map((datum) => {
-						if (datum.hasOwnProperty('link')) {
-							const { id, name, link, target, rel } = datum;
-							return (
-								<a
-									key={id}
-									className={name}
-									href={link}
-									target={target}
-									rel={rel}
-								>
-									{name}
-								</a>
-							);
-						} else {
-							const { id, name, classname } = datum;
-							return (
-								<>
-									<h2 key={id} className={name}>
-										{name}
-									</h2>
-									<div className={classname}></div>
-								</>
-							);
-						}
+					{links.map((sLink) => {
+						const { id, name, link, target, rel } = sLink;
+
+						return (
+							<a
+								key={id}
+								className={name}
+								href={link}
+								target={target}
+								rel={rel}
+							>
+								{name}
+							</a>
+						);
 					})}
+				</div>
+
+				<div className='col2'>
+					{projects.map((project) => {
+						const { id, name, link, target, rel } = project;
+
+						return (
+							<a
+								key={id}
+								className={name}
+								href={link}
+								target={target}
+								rel={rel}
+							>
+								{name}
+							</a>
+						);
+					})}
+				</div>
+				<div className='col3'>
 					<a
 						href='https://github.com/ousmanebarry/me'
 						target='_blank'
@@ -39,7 +48,7 @@ const Main = () => {
 						className='source-code'
 					>
 						<img
-							src='https://cdn.discordapp.com/attachments/439833184260063233/926272727260741722/linking_64px.png'
+							src='linking_64px.png'
 							alt='Redirect Link'
 							width='40px'
 							height='40px'
