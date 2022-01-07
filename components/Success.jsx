7320c8e9940297015b8dc5ea41f14e5fa22f0466
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2022-01-07 06:07:45 -0500

    200 Handling

--- components/Success.jsx
+++ components/Success.jsx
@@ -4,7 +4,12 @@ export default function Success({ btn_close }) {
 	return (
 		<div className={styles.fields}>
 			<div className={styles.success}>
-				<p>Message was successfully sent!</p>
+				<p>
+					<span>
+						<img src='/checkmark.svg' alt='' />
+					</span>
+					Message was successfully sent!
+				</p>
 			</div>
 			<button className={styles.btn} onClick={btn_close}>
 				Close
