674e05cb2e84060549796ecbdcc909c690f9cea3
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2022-01-06 23:57:48 -0500

    Update Form.jsx

--- pages/Form.jsx
+++ pages/Form.jsx
@@ -1,21 +1,62 @@
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+import Link from 'next/link';
 
 export default function Form() {
+	const [chars, setChars] = useState(0);
+
 	return (
 		<>
 			<div className={styles.fields}>
 				<form action='/' method='post'>
-					<input name='name' placeholder='Name*' type='text' value=''></input>
-					<p>Please enter a name</p>
+					<input
+						name='name'
+						placeholder='Full name*'
+						type='text'
+						className={styles.name}
+						autoComplete='off'
+						required
+					></input>
+
 					<input
 						name='email'
 						placeholder='Email address*'
 						type='text'
-						value=''
+						className={styles.email}
+						autoComplete='off'
+						required
 					></input>
-					<p>Please enter a valid email address</p>
-					<input name='name' placeholder='Name*' type='text' value=''></input>
-					<p>Please enter a name</p>
+
+					<textarea
+						name='message'
+						placeholder='Message*'
+						maxLength='500'
+						type='text'
+						className={styles.message}
+						autoComplete='off'
+						onChange={(e) => setChars(e.target.value.length)}
+						required
+					></textarea>
+
+					<p className={styles.chars_count}>{chars}/500</p>
+
+					<div className={styles.consent_div}>
+						<p className={styles.consent}>
+							By submitting your contact details, you are providing your data to
+							Ousmane Barry who may contact you. You can alternatively send me
+							an email at{' '}
+							<span>
+								<Link href='mailto:kegnekobarry822@gmail.com'>
+									kegnekobarry822@gmail.com
+								</Link>
+							</span>
+							.
+						</p>
+					</div>
+
+					<button type='submit' className={styles.submit}>
+						Send
+					</button>
 				</form>
 			</div>
 		</>
