01a2dbd3dff6a36d7505e5de2e01f4bb7d3ff508
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2022-02-14 16:05:57 -0500

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -12,112 +12,89 @@ import styles from '../styles/Home.module.css';
 export const ThemeContext = React.createContext();
 
 export default function Home() {
-	const [clickedMail, setClickedMail] = useState(false);
-	const [clickedGithub, setClickedGithub] = useState(false);
-	const [sent, setSent] = useState(false);
-	const effectMail = useRef();
-	const effectGithub = useRef();
+  const [clickedMail, setClickedMail] = useState(false);
+  const [clickedGithub, setClickedGithub] = useState(false);
+  const [sent, setSent] = useState(false);
+  const effectMail = useRef();
+  const effectGithub = useRef();
 
-	const checkClicked = (btn) => {
-		if (btn === 'github') {
-			setClickedGithub(!clickedGithub);
-			effectGithub.current.classList.toggle(styles.effect);
-		} else {
-			setClickedMail(!clickedMail);
-			effectMail.current.classList.toggle(styles.effect);
-		}
-	};
+  const checkClicked = (btn) => {
+    if (btn === 'github') {
+      setClickedGithub(!clickedGithub);
+      effectGithub.current.classList.toggle(styles.effect);
+    } else {
+      setClickedMail(!clickedMail);
+      effectMail.current.classList.toggle(styles.effect);
+    }
+  };
 
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Ousmane Barry</title>
-				<link rel='icon' href='a.png' />
-			</Head>
-			<div className={styles.box}>
-				<main className={styles.main}>
-					<div className={styles.head}>
-						<img
-							src='/profile.jpg'
-							alt='Profile'
-							className={styles.image}
-						></img>
-						<h1 className={styles.title}>Ousmane Barry</h1>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Ousmane Barry</title>
+        <link rel='icon' href='a.png' />
+      </Head>
+      <div className={styles.box}>
+        <main className={styles.main}>
+          <div className={styles.head}>
+            <img src='/profile.jpg' alt='Profile' className={styles.image}></img>
+            <h1 className={styles.title}>Ousmane Barry</h1>
 
-						<p className={styles.description}>
-							Software Engineering Student at the University of Ottawa
-						</p>
-					</div>
+            <p className={styles.description}>Software Engineering Student at the University of Ottawa</p>
+          </div>
 
-					<div
-						className={styles.github}
-						onClick={() => checkClicked('github')}
-						ref={effectGithub}
-					>
-						<p>Github</p>
-						<Icon className={styles.arrow_down} icon='il:arrow-down' />
-					</div>
+          <div className={styles.github} onClick={() => checkClicked('github')} ref={effectGithub}>
+            <p>Github</p>
+            <Icon className={styles.arrow_down} icon='il:arrow-down' />
+          </div>
 
-					{clickedGithub ? <Repos /> : <Null />}
+          {clickedGithub ? <Repos /> : <Null />}
 
-					<div className={styles.resume}>
-						<Link href='https://firebasestorage.googleapis.com/v0/b/redirect-links-3a125.appspot.com/o/resume-ousmane%20shared.pdf?alt=media&token=f4214aec-0fe9-46a7-b32b-264e8caf2b78'>
-							<a target='_blank'>
-								<p>Resume</p>
-								<Icon
-									className={styles.external}
-									icon='fa-solid:external-link-alt'
-								/>
-							</a>
-						</Link>
-					</div>
+          <div className={styles.resume}>
+            <Link href='https://firebasestorage.googleapis.com/v0/b/redirect-links-3a125.appspot.com/o/resume-ousmane%20shared.pdf?alt=media&token=f4214aec-0fe9-46a7-b32b-264e8caf2b78'>
+              <a target='_blank'>
+                <p>Resume</p>
+                <Icon className={styles.external} icon='fa-solid:external-link-alt' />
+              </a>
+            </Link>
+          </div>
 
-					<div className={styles.portfolio}>
-						<Link href='https://ousmanebarry.ca/'>
-							<a target='_blank'>
-								<p>Portfolio</p>
-								<Icon
-									className={styles.external}
-									icon='fa-solid:external-link-alt'
-								/>
-							</a>
-						</Link>
-					</div>
+          <div className={styles.portfolio}>
+            <Link href='https://ousmanebarry.ca/'>
+              <a target='_blank'>
+                <p>Portfolio</p>
+                <Icon className={styles.external} icon='fa-solid:external-link-alt' />
+              </a>
+            </Link>
+          </div>
 
-					<div className={styles.linkedin}>
-						<Link href='https://www.linkedin.com/in/barry-ousmane/'>
-							<a target='_blank'>
-								<p>LinkedIn</p>
-								<Icon
-									className={styles.external}
-									icon='fa-solid:external-link-alt'
-								/>
-							</a>
-						</Link>
-					</div>
+          <div className={styles.linkedin}>
+            <Link href='https://www.linkedin.com/in/barry-ousmane/'>
+              <a target='_blank'>
+                <p>LinkedIn</p>
+                <Icon className={styles.external} icon='fa-solid:external-link-alt' />
+              </a>
+            </Link>
+          </div>
 
-					<div
-						className={styles.mail}
-						ref={effectMail}
-						onClick={() => checkClicked('mail')}
-					>
-						<p>Contact Me</p>
-						<Icon className={styles.arrow_down} icon='il:arrow-down' />
-					</div>
+          <div className={styles.mail} ref={effectMail} onClick={() => checkClicked('mail')}>
+            <p>Contact Me</p>
+            <Icon className={styles.arrow_down} icon='il:arrow-down' />
+          </div>
 
-					{clickedMail ? (
-						sent ? (
-							<Success btn_close={() => checkClicked('mail')} />
-						) : (
-							<ThemeContext.Provider value={setSent}>
-								<Form />
-							</ThemeContext.Provider>
-						)
-					) : (
-						<Null />
-					)}
-				</main>
-			</div>
-		</div>
-	);
+          {clickedMail ? (
+            sent ? (
+              <Success btn_close={() => checkClicked('mail')} />
+            ) : (
+              <ThemeContext.Provider value={setSent}>
+                <Form />
+              </ThemeContext.Provider>
+            )
+          ) : (
+            <Null />
+          )}
+        </main>
+      </div>
+    </div>
+  );
 }
